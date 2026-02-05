a59c57d5ff94464b049316f4082e6ecb1db6da9a
NoneT2T <zelezovdaniil9@gmail.com>
2026-02-05 21:50:11 +0300

    Базовые настройки

--- Screensaver/Form1.cs
+++ Screensaver/Form1.cs
@@ -2,8 +2,40 @@ namespace Screensaver;
 
 public partial class Form1 : Form
 {
+    private System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer() { Interval = 20 };
     public Form1()
     {
         InitializeComponent();
+        SetupForm();
+
+        timer.Tick += (s, e) => UpdateSnowflakes();
+        this.Paint += (s, e) => DrawScene(e.Graphics);
+
+        this.KeyDown += (s, e) => Close();
+        this.MouseClick += (s, e) => Close();
+    }
+
+    private void SetupForm()
+    {
+        this.FormBorderStyle = FormBorderStyle.None;
+        this.WindowState = FormWindowState.Maximized;
+        this.BackColor = Color.Black;
+        this.SetStyle(ControlStyles.UserPaint, true);
+    }
+
+    private void UpdateSnowflakes()
+    {
+        
+    }
+
+    private void DrawScene(Graphics graphics)
+    {
+        
+    }
+
+    protected override void OnFormClosing(FormClosingEventArgs e)
+    {
+        timer.Stop();
+        base.OnFormClosing(e);
     }
 }
